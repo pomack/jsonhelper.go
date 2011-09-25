b1d7d3b0145011bab9e98afe1b76ab2387f3688d
Aalok Shah <aalok@pomack.com>
2011-09-25 00:45:40 -0400

    Format with gofmt but using spaces not tabs

--- jsonarray.go
+++ jsonarray.go
@@ -1,54 +1,54 @@
 package jsonhelper
 
 import (
-	"time"
+    "time"
 )
 
 type JSONArray []interface{}
 
 func NewJSONArray() JSONArray {
-	return make([]interface{}, 0)
+    return make([]interface{}, 0)
 }
 
 func NewJSONArrayFromArray(value []interface{}) JSONArray {
-	return JSONArray(value)
+    return JSONArray(value)
 }
 
 func (p JSONArray) Len() int {
-	return len(p)
+    return len(p)
 }
 
 func (p JSONArray) GetAsString(index int) string {
-	value := p[index]
-	return JSONValueToString(value)
+    value := p[index]
+    return JSONValueToString(value)
 }
 
 func (p JSONArray) GetAsInt(index int) int {
-	value := p[index]
-	return JSONValueToInt(value)
+    value := p[index]
+    return JSONValueToInt(value)
 }
 
 func (p JSONArray) GetAsInt64(index int) int64 {
-	value := p[index]
-	return JSONValueToInt64(value)
+    value := p[index]
+    return JSONValueToInt64(value)
 }
 
 func (p JSONArray) GetAsFloat64(index int) float64 {
-	value := p[index]
-	return JSONValueToFloat64(value)
+    value := p[index]
+    return JSONValueToFloat64(value)
 }
 
 func (p JSONArray) GetAsObject(index int) JSONObject {
-	value := p[index]
-	return JSONValueToObject(value)
+    value := p[index]
+    return JSONValueToObject(value)
 }
 
 func (p JSONArray) GetAsArray(index int) JSONArray {
-	value := p[index]
-	return JSONValueToArray(value)
+    value := p[index]
+    return JSONValueToArray(value)
 }
 
 func (p JSONArray) GetAsTime(index int, format string) *time.Time {
-	value := p[index]
-	return JSONValueToTime(value, format)
+    value := p[index]
+    return JSONValueToTime(value, format)
 }
--- jsonobject.go
+++ jsonobject.go
@@ -1,68 +1,68 @@
 package jsonhelper
 
 import (
-	"time"
+    "time"
 )
 
 type JSONObject map[string]interface{}
 
 func NewJSONObject() JSONObject {
-	return make(JSONObject)
+    return make(JSONObject)
 }
 
 func NewJSONObjectFromMap(m map[string]interface{}) JSONObject {
-	return JSONObject(m)
+    return JSONObject(m)
 }
 
 func (p JSONObject) Del(key string) {
-	p[key] = nil, false
+    p[key] = nil, false
 }
 
 func (p JSONObject) Set(key string, value interface{}) {
-	p[key] = value
+    p[key] = value
 }
 
 func (p JSONObject) Get(key string) interface{} {
-	value, _ := p[key]
-	return value
+    value, _ := p[key]
+    return value
 }
 
 func (p JSONObject) GetAsString(key string) string {
-	value, _ := p[key]
-	return JSONValueToString(value)
+    value, _ := p[key]
+    return JSONValueToString(value)
 }
 
 func (p JSONObject) GetAsInt(key string) int {
-	value, _ := p[key]
-	return JSONValueToInt(value)
+    value, _ := p[key]
+    return JSONValueToInt(value)
 }
 
 func (p JSONObject) GetAsInt64(key string) int64 {
-	value, _ := p[key]
-	return JSONValueToInt64(value)
+    value, _ := p[key]
+    return JSONValueToInt64(value)
 }
 
 func (p JSONObject) GetAsFloat64(key string) float64 {
-	value, _ := p[key]
-	return JSONValueToFloat64(value)
+    value, _ := p[key]
+    return JSONValueToFloat64(value)
 }
 
 func (p JSONObject) GetAsBool(key string) bool {
-	value, _ := p[key]
-	return JSONValueToBool(value)
+    value, _ := p[key]
+    return JSONValueToBool(value)
 }
 
 func (p JSONObject) GetAsObject(key string) JSONObject {
-	value, _ := p[key]
-	return JSONValueToObject(value)
+    value, _ := p[key]
+    return JSONValueToObject(value)
 }
 
 func (p JSONObject) GetAsArray(key string) JSONArray {
-	value, _ := p[key]
-	return JSONValueToArray(value)
+    value, _ := p[key]
+    return JSONValueToArray(value)
 }
 
 func (p JSONObject) GetAsTime(key string, format string) *time.Time {
-	value, _ := p[key]
-	return JSONValueToTime(value, format)
+    value, _ := p[key]
+    return JSONValueToTime(value, format)
 }
